be22d9c6d43a92fc51d591e50494ad76b71c4ef2
warramma <wishmorethewitch@gmail.com>
2024-07-11 20:57:07 -0500

    Rename to ArrayStack and implement stack functions

---
+++ ArrayStack/src/ArrayStack.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ramma
+ */
+public class ArrayStack<T> {
+    private int top;
+    private T[] stack;
+    private final int MAX = 100;
+    
+    public ArrayStack(){
+        //this is a default constructor, no parameters
+        top = 0;
+        stack = (T[])(new Object[MAX]);
+    }
+    
+    public void push(T newElement){
+        stack[top] = newElement;
+        top++;
+    }
+    
+    public T pop(){
+        T result = stack[top];
+        top--;
+        return result;
+    }
+    
+    public T peek(){
+        return stack[top];
+    }
+}
