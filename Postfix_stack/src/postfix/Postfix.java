331fcb53b382ac7bb993a3a6aa3b04aac66797e8
warramma <wishmorethewitch@gmail.com>
2024-07-12 16:16:00 -0500

    Change numbers type from Integer to Float

--- Postfix_stack/src/postfix/Postfix.java
+++ Postfix_stack/src/postfix/Postfix.java
@@ -14,7 +14,7 @@ import stack.ArrayStack;
 import java.util.Scanner;
 
 public class Postfix<T> {
-    ArrayStack<Integer> integers = new ArrayStack<>();
+    ArrayStack<Float> numbers = new ArrayStack<>();
     ArrayStack<Character> operators = new ArrayStack<>();
     public void loadExpression(String expression){
         String token;
@@ -24,16 +24,44 @@ public class Postfix<T> {
             if(isOperator(token)){
                 operators.push(token.charAt(0));
             }else{
-                integers.push(Integer.valueOf(token));
+                numbers.push(Float.valueOf(token));
             }
         }
     }
     public void displayExpression(){
-        integers.displayStack();
+        numbers.displayStack();
         operators.displayStack();
         System.out.println();
     }
-    
+    public Float evaluateExpression(){
+        while (numbers.getTop() != 1){
+            char operator = operators.pop();
+            float num2 = numbers.pop();
+            float num1 = numbers.pop();
+            switch (operator) {
+                case '+':
+                    numbers.push(num1 + num2);
+                    break;
+                case '-':
+                    numbers.push(num1 - num2);
+                    break;
+                case '/':
+                    if(num1 > 0 && num2 == 0){
+                        String error = "Error: " + num1 + " / " + num2 + " is undefined";
+                        System.out.println(error);
+                    }
+                    numbers.push(num1 / num2);
+                    break;
+                case 'x':
+                    numbers.push(num1 * num2);
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        return numbers.peek();
+    }
     private boolean isOperator(String token){
         boolean result = false;
         if(token.equals("+") || token.equals("-") || token.equals("/") || token.equals("x")){
