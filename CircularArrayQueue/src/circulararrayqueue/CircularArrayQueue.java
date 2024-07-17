664e7696c4722b43ddff3634b3d2a09c9973a747
warramma <wishmorethewitch@gmail.com>
2024-07-17 16:33:36 -0500

    Add files

---
+++ CircularArrayQueue/src/circulararrayqueue/CircularArrayQueue.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package circulararrayqueue;
+
+/**
+ *
+ * @author ramma
+ * @param <T>
+ */
+public class CircularArrayQueue<T> implements QueueADT<T>{
+
+    @Override
+    public void queue(T elem) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T dequeue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T first() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public String toString(){
+        return "";
+    };
+}
---
+++ CircularArrayQueue/src/circulararrayqueue/QueueADT.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package circulararrayqueue;
+
+/**
+ *
+ * @author ramma
+ */
+public interface QueueADT<T>{
+    public void queue(T elem);
+    public T dequeue();
+    public T first();
+    public int size();
+    public boolean isEmpty();
+    @Override
+    public String toString();
+}
